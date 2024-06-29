5204f55bca17355daee63be24a0618f96558a159
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2024-06-29 08:48:47 +0300

    js

---
+++ Hoisting/hoist.js
@@ -0,0 +1,22 @@
+
+
+const summ =(a,b)=>a+b
+
+console.log(sum(2,3))
+console.log(summ(5,6))
+function sum(a,b){
+    return a+b
+}
+// Any function def by function key word is moved to the top when js starts executing code
+// this means the function with the function keyword can be written after calling it and it will work fine
+//visevarsa for the const keyword
+//var key word is also hoisted but it is not for a function
+//if you def a variable using var js hoists it to top and assigns undefined to it 
+//so
+
+console.log(a)
+var a=2
+
+//returns undefined
+//var is not a good way of def variables
+
